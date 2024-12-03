8587bbab0698005391cf00dc61e68abeb38c3ec6
parkyeseung <yeseung0610@gmail.com>
2024-12-03 18:12:34 +0900

    [FEAT] - 20241203 프로젝트 기본 세팅 완료

---
+++ src/main/java/com/example/handy_home/HandyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.handy_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyHomeApplication.class, args);
+	}
+
+}
