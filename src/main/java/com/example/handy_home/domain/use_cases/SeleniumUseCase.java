76bcf8f27c3f42e5c1c1b47649e48ffcd5797e74
parkyeseung <yeseung0610@gmail.com>
2024-12-24 21:20:28 +0900

    FEAT - [20241224] REG_002 기능 개발 완료

--- src/main/java/com/example/handy_home/domain/use_cases/SeleniumUseCase.java
+++ src/main/java/com/example/handy_home/domain/use_cases/SeleniumUseCase.java
@@ -1,27 +1,82 @@
 package com.example.handy_home.domain.use_cases;
 
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.logging.Log;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class SeleniumUseCase {
 
     static private String WEB_DRIVER_ID = "webdriver.chrome.driver";
-    static private String WEB_DRIVER_PATH = "static/chromedriver.exe";
+    static private String WEB_DRIVER_PATH = "static/chromedriver";
 
-    public List<String> searchKeywordInNaverRealty(String keyword) {
+    static private String WEB_BASE_URL = "https://new.land.naver.com/complexes?ms=37.3595704,127.105399,16&a=APT:ABYG:JGC:PRE&e=RETAIL";
+
+    WebDriver driver;
+
+    public SeleniumUseCase() {
+        initWebDriver();
+    }
+
+    private void initWebDriver() {
         System.setProperty(WEB_DRIVER_ID, Objects.requireNonNull(getClass().getClassLoader().getResource(WEB_DRIVER_PATH)).getPath());
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("headless");
 
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+        driver.get(WEB_BASE_URL);
+    }
+
+    public List<String> searchKeywordInNaverRealty(String keyword) {
+        if (driver == null) {
+            initWebDriver();
+        }
+
+        if (!Objects.equals(driver.getCurrentUrl(), WEB_BASE_URL)) {
+            driver.get(WEB_BASE_URL);
+        }
+
+        try {
+            WebElement searchInput = driver.findElement(By.id("land_search"));
+            searchInput.sendKeys(keyword);
+            searchInput.sendKeys(Keys.ENTER);
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("list_contents")));
+
+            List<WebElement> itemList = driver.findElements(By.cssSelector(".item_area .item"));
+
+            final List<String> resultList = new ArrayList<>();
+
+            for (WebElement item : itemList) {
+                WebElement itemLink = item.findElement(By.cssSelector(".item_inner .item_link"));
+                String type = itemLink.findElement(By.cssSelector(".info_area .type")).getText();
+
+                if (type.contains("아파트") || type.contains("오피스텔")) {
+                    WebElement titleElement = item.findElement(By.cssSelector(".item_inner .title"));
+                    resultList.add(titleElement.getText());
+                }
+            }
+
+            return resultList;
+        } catch (Exception e) {
+            return new ArrayList<>();
+        } finally {
+            driver.get(WEB_BASE_URL);
+        }
     }
 
 }
--- src/main/java/com/example/handy_home/presentation/controllers/HomeController.java
+++ src/main/java/com/example/handy_home/presentation/controllers/HomeController.java
@@ -3,14 +3,17 @@ package com.example.handy_home.presentation.controllers;
 import com.example.handy_home.domain.use_cases.SeleniumUseCase;
 import com.example.handy_home.presentation.dto.response.SearchAddressResponseDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "002. Home")
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/home")
 public class HomeController {
 
     private final SeleniumUseCase seleniumUseCase;
@@ -19,8 +22,9 @@ public class HomeController {
         this.seleniumUseCase = seleniumUseCase;
     }
 
+    @GetMapping
     public ResponseEntity<SearchAddressResponseDTO> searchAddress(@RequestParam("keyword") String keyword) {
-
+        return ResponseEntity.ok(new SearchAddressResponseDTO(seleniumUseCase.searchKeywordInNaverRealty(keyword)));
     }
 
 }
