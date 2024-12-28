9e094b937fdaf12b0c04ce3a2dbfc3dbfe990b8a
jisu han <haanjisu@gmail.com>
2024-12-28 19:08:13 +0900

    FEAT - [20241228] REG_002 타이틀 반환 로직에 주소 추가

--- src/main/java/com/example/handy_home/presentation/dto/response/SearchAddressResponseDTO.java
+++ src/main/java/com/example/handy_home/presentation/dto/response/SearchAddressResponseDTO.java
@@ -3,9 +3,10 @@ package com.example.handy_home.presentation.dto.response;
 import com.example.handy_home.common.utils.Status;
 
 import java.util.List;
+import java.util.Map;
 
-public class SearchAddressResponseDTO extends ResponseDTO<List<String>> {
-    public SearchAddressResponseDTO(List<String> body) {
+public class SearchAddressResponseDTO extends ResponseDTO<List<Map<String, String>>> {
+    public SearchAddressResponseDTO(List<Map<String, String>> body) {
         super(Status.S0000, true, body);
     }
 }
